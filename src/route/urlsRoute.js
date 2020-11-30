8bc59c07c20b95a3e11e028a7e3ccdd6375d246e
blackALT <34174851+blackALT@users.noreply.github.com>
2020-11-30 18:34:57 -0300

    update routes

--- src/route/urlsRoute.js
+++ src/route/urlsRoute.js
@@ -1,13 +1,19 @@
 const express = require("express")
 const router = express.Router()
+require('dotenv-safe').config();
 const controller = require("../controller/urlsController")
 
-router.get("/", controller.getUrls);
-router.get("/:target", controller.getTarget)
-router.get("/submissions", controller.getSubmissions);
-router.post("/submissions", controller.postUrl);
-router.put("/:id", controller.putUrl);
-router.put("/:id", controller.deleteUrl);
-router.put("/:id", controller.patchUrl);
+// Rotas não autenticadas
+router.get("/", controller.getAnalyzed); // Apenas analisadas
+router.get("/malicious", controller.getMalicious); // Alvo analisado e malicioso
+router.post("/", controller.postUrl);
+
+// Rotas autenticadas
+router.get("/submissions", controller.getSubmissions); // Apenas não analisadas
+router.get("/backlog", controller.getBacklog); // retorna todas
+router.put("/submissions/:id", controller.putUrl);
+router.delete("/submissions/:id", controller.deleteByID);
+router.delete("/submissions", controller.deleteByUrl);
+router.patch("/submissions/:id", controller.patchUrl);
 
 module.exports = router
