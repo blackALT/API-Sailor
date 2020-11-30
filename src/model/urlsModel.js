29b8bf74ec11b7e4542ea16f20eff7f0a30e61f0
blackALT <34174851+blackALT@users.noreply.github.com>
2020-11-30 17:07:07 -0300

    Update model

--- src/model/urlsModel.js
+++ src/model/urlsModel.js
@@ -1,12 +1,32 @@
 const mongoose = require('mongoose');
 
 const urlSchema = new mongoose.Schema({
-    id: { type: Number },
-    url: { type: String },
-    target: { type: String },
-    submissionDate: { type: String },
-    isAnalyzed: {type: Boolean},
-    isMalicious: {type: Boolean}
+    id: { 
+        type: Number 
+    },
+    url: { 
+        type: String, 
+        required: true 
+    },
+    target: { 
+        type: String, 
+        default: "",
+    },
+    submissionDate: { 
+        type: Date, 
+        default: Date.now, 
+        required: true 
+    },
+    isAnalyzed: {
+        type: Boolean, 
+        default: false, 
+        required: true
+    },
+    isMalicious: {
+        type: Boolean, 
+        default: false, 
+        required: true
+    }
 }, {
     versionKey: false
 })
